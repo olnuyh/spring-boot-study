4497e4c526c499ec27082272d1ce3a3553e4ee50
김이현 <rihyun1126@gmail.com>
2023-12-14 14:09:26 +0900

    [5장] 게시판 Read 기능 구현

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -33,5 +37,23 @@ public class MemberController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member member = memberRepository.findById(id).orElse(null);
+
+        model.addAttribute("member", member);
+
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        Iterable<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+
+        return "members/index";
     }
 }
