97157d2f2d0db270a88b61252c8ea2392f4ea44b
김이현 <rihyun1126@gmail.com>
2023-12-14 13:32:16 +0900

    [4장] lombok으로 코드 리팩토링

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
@@ -20,11 +22,15 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(MemberForm form){
+        log.info(form.toString());
+
         // 1. DTO를 Entity로 변경
         Member member = form.toEntity();
+        log.info(member.toString());
 
         // 2. Repository를 통해 Entity를 DB에 저장
         Member saved = memberRepository.save(member);
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Member {
     @Id
     @GeneratedValue
@@ -14,19 +20,4 @@ public class Member {
     private String email;
     @Column
     private String password;
-
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
